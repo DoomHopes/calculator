856d3ba1b6e149be037558810ba113fd4f290893
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-11-01 11:09:19 +0200

    add colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 
 class MyColors {
   static const Color backgroung1 = Color(0xFF2B333D);
+  static const Color buttonColor1 = Color(0xFF2C303A);
+  static const Color buttonIconColor1 = Color(0xFFB0FFF6);
+  static const Color buttonIconColor2 = Color(0xFFE8A0A6);
+  static const Color buttonIconColor3 = Color(0xFFB0FFF6);
 }

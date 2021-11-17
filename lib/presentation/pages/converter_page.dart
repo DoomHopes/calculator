8b3e01e29d630d50c3de7007dff8f409963272a5
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-11-17 16:11:04 +0200

    add tab bar

--- lib/presentation/pages/converter_page.dart
+++ lib/presentation/pages/converter_page.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-class ConverterPage extends StatelessWidget {
+class ConverterPage extends StatefulWidget {
   const ConverterPage({Key? key}) : super(key: key);
+
+  @override
+  State<ConverterPage> createState() => _ConverterPageState();
+}
+
+class _ConverterPageState extends State<ConverterPage>
+    with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 8, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +23,68 @@ class ConverterPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Converter'),
       ),
-      // TODO body
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 60,
+            child: TabBar(
+              isScrollable: true,
+              controller: _tabController,
+              tabs: const <Widget>[
+                Text(
+                  'Square',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  'Length',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  'Temperature',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  'Volume',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  'Weight',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  'Data',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  'Speed',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                Text(
+                  'Time',
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class TapBarItem extends StatelessWidget {
+  const TapBarItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }

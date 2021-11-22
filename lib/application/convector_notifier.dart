645582610f76cb2d350bdb292e2d0d15aead1476
DoomHopes <63532660+DoomHopes@users.noreply.github.com>
2021-11-22 16:49:19 +0200

    some change

--- lib/application/convector_notifier.dart
+++ lib/application/convector_notifier.dart
@@ -1,15 +1,13 @@
 import 'package:calculator/presentation/utils/drop_down_elements.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:converter/converter.dart';
+import 'package:flutter/cupertino.dart';
 
 class ConvectorNotifier extends ChangeNotifier {
   Set<List<String>> dropDownElements = {
     MyDropDownElements.squares,
     MyDropDownElements.length,
     MyDropDownElements.temperature,
-    //MyDropDownElements.volume,
     MyDropDownElements.weight,
-    //MyDropDownElements.data,
     MyDropDownElements.speed,
     MyDropDownElements.time,
   };
